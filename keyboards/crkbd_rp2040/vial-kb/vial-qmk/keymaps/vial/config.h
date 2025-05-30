9cda320cc21359b6c95771766008c1e9eb6124f6
miguelbaldi <miguel.horlle@gmail.com>
2025-05-30 00:49:22 -0300

    Corne V3 (PandaKB Gateron Low Profile) support.
      - Bilateral combinations enabled.
      - Vial enabled
      - RP2040 enabled
      - Custom screen support enabled.

---
+++ keyboards/crkbd_rp2040/vial-kb/vial-qmk/keymaps/vial/config.h
@@ -0,0 +1,43 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3B, 0x6B, 0xA0, 0x29, 0x80, 0x56, 0xED, 0xD1}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+#define VIAL_COMBO_ENTRIES 64
+#define VIAL_KEY_OVERRIDE_ENTRIES 64
+#define VIAL_TAP_DANCE_ENTRIES 64
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+// #define VIALRGB_NO_DIRECT
+
+/* QMK */
+#define TAPPING_TERM 200
+
+/* Miryoku */
+#define BILATERAL_COMBINATIONS
+#define BILATERAL_COMBINATIONS_LIMIT_CHORD_TO_N_KEYS 4 /* GUI, Alt, Ctrl, Shift */
+#define BILATERAL_COMBINATIONS_DELAY_MODS_THAT_MATCH MOD_MASK_GUI
+#define BILATERAL_COMBINATIONS_DELAY_MATCHED_MODS_BY 120  /* ms */
+#define BILATERAL_COMBINATIONS_ALLOW_CROSSOVER_AFTER 80   /* ms */
+#define BILATERAL_COMBINATIONS_ALLOW_SAMESIDED_AFTER 3000 /* ms */
+#define BILATERAL_COMBINATIONS_TYPING_STREAK_TIMEOUT 160  /* ms */
+#define BILATERAL_COMBINATIONS_TYPING_STREAK_MODMASK (~MOD_MASK_SHIFT)
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 This fork is intended to integrate the **amazing** [sunaku](https://github.com/sunaku) work on QMK [bilateral combinations](https://sunaku.github.io/home-row-mods.html),
 allowing an improved experience using [home row mods](https://precondition.github.io/home-row-mods) with Vial on Corne v4.1.
 
-On this repository, **only v4.1 standard Vial** configuration has bilateral combinations customization applied.
+On this repository, **only v4.1 standard Vial** and **rp2040 v3** configurations has bilateral combinations customization applied.
 
 To achieve such integration, I've forked vial-qmk and ported sunako's patch into it: [vial-qmk](https://github.com/miguelbaldi/vial-qmk)
 
@@ -44,6 +44,13 @@ kb=crkbd kr=rev4_1/standard km=vial make vial-qmk-compile
 ```
 A built data will be stored on `keyboards/crkbd/vial-kb/vial-qmk/.build`\
 Please change `kb`, `kr` and `km` when build other.
+
+For V3 (PandaKB Gateron Low Profile)
+```sh
+make vial-qmk-clean
+kb=crkbd_rp2040 make vial-qmk-init
+kb=crkbd_rp2040 km=vial make vial-qmk-compile
+```
 
 ### All cleaning and building
 ```sh
